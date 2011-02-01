fba8b4661b2c6f1d336ee76d08747f3edb547457
Tomas Kramar <kramar.tomas@gmail.com>
2011-02-01 11:51:17 +0100

    Add xbindkeys config

---
+++ .xbindkeysrc.scm
@@ -0,0 +1,4 @@
+(xbindkey '("XF86AudioRaiseVolume") "/etc/acpi/actions/volume up")
+(xbindkey '("XF86AudioLowerVolume") "/etc/acpi/actions/volume down")
+(xbindkey '("XF86AudioMute") "/etc/acpi/actions/mute")
+
