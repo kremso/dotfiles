6b1cc37e3a13ba20ede8e2c13db6c5a0bca0eb7d
Tomas Kramar <kramar.tomas@gmail.com>
2011-08-04 21:06:47 +0200

    Fix hotkeys on my new laptop

--- .xbindkeysrc.scm
+++ .xbindkeysrc.scm
@@ -1,4 +1,5 @@
-(xbindkey '("XF86AudioRaiseVolume") "/etc/acpi/actions/volume up")
-(xbindkey '("XF86AudioLowerVolume") "/etc/acpi/actions/volume down")
-(xbindkey '("XF86AudioMute") "/etc/acpi/actions/mute")
+(xbindkey '("XF86AudioRaiseVolume") "amixer set Master 1+")
+(xbindkey '("XF86AudioLowerVolume") "amixer set Master 5-")
+(xbindkey '("XF86AudioMute") "amixer set Master 0")
+(xbindkey '("XF86Sleep") "sudo pm-suspend")
 
