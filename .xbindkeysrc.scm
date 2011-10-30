c46ff7aaec27ec71d059c97396f46dd2649786c9
Tomas Kramar <kramar.tomas@gmail.com>
2011-10-30 17:28:48 +0100

    Fix the volume hotkeys

--- .xbindkeysrc.scm
+++ .xbindkeysrc.scm
@@ -1,5 +1,5 @@
-(xbindkey '("XF86AudioRaiseVolume") "amixer set Master 1+")
-(xbindkey '("XF86AudioLowerVolume") "amixer set Master 5-")
-(xbindkey '("XF86AudioMute") "amixer set Master 0")
+(xbindkey '("XF86AudioRaiseVolume") "amixer set Master 5%+")
+(xbindkey '("XF86AudioLowerVolume") "amixer set Master 5%-")
+(xbindkey '("XF86AudioMute") "amixer set Master 0%")
 (xbindkey '("XF86Sleep") "sudo pm-suspend")
 
