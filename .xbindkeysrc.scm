489adeb14e8643076cf1b7d35ec4322db0e70be4
Tomas Kramar <kramar.tomas@gmail.com>
2013-11-11 11:50:21 +0100

    Remove configs I don't use anymore

--- .xbindkeysrc.scm
+++
@@ -1,7 +0,0 @@
-(xbindkey '("XF86AudioRaiseVolume") "amixer set Master 5%+")
-(xbindkey '("XF86AudioLowerVolume") "amixer set Master 5%-")
-(xbindkey '("XF86AudioMute") "amixer set Master 0%")
-(xbindkey '("XF86MonBrightnessDown") "~/dotfiles/bin/screen_control.sh down")
-(xbindkey '("XF86MonBrightnessUp") "~/dotfiles/bin/screen_control.sh up")
-(xbindkey '("XF86Sleep") "sudo pm-suspend")
-
