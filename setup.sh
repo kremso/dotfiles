612daaa541c3d06ccd50bb14bd8d667ed76f148d
Tomáš Kramár <kramar.tomas@gmail.com>
2016-11-29 11:25:42 +0100

    Add simple setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ln -s ~/.dotfiles/.config/fish ~/.config/
+ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/.config/nvim/*.vim ~/.config/nvim
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/.githelpers ~/.githelpers
+ln -s ~/.dotfiles/.git_template ~/.git_template
