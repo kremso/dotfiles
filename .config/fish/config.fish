22ae8a254c2bb1804fcd1ca49803933b1c065315
Tomáš Kramár <kramar.tomas@gmail.com>
2016-11-29 11:25:13 +0100

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,20 +1,23 @@
-set -gx PATH $PATH "$HOME/.builds/bin"
-set -gx PATH $PATH "$HOME/.local/bin"
-
 set -g -x fish_term24bit 1
 
 set BROWSER open
 
 set -g -x fish_greeting ''
 set -g -x EDITOR nvim
+set -g -x GOPATH "$HOME/Projects/go"
+
+set -U fish_user_paths $HOME/Projects/go/bin/ $fish_user_paths
 
 alias gst="git status"
 alias gc="git commit -v"
 alias gp="git push"
 alias vim="nvim"
+alias mux="tmuxinator"
 
-set -gx PATH "$HOME/.rbenv/bin" $PATH
-. (rbenv init -|psub)
+if test -d "$HOME/.rbenv"
+  set -gx PATH "$HOME/.rbenv/bin" $PATH
+  . (rbenv init -|psub)
+end
 
 set -gx Z_SCRIPT_PATH ~/.config/fish/tools/z.sh
 
