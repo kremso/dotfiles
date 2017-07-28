7b98645d53c4f1e0856e8d402c1274a612235297
Tomáš Kramár <kramar.tomas@gmail.com>
2017-07-28 23:42:33 +0200

    fish: add color config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -27,3 +27,30 @@ function __check_z --on-variable PWD --description 'Setup z on directory change'
 
   bash -c "source $Z_SCRIPT_PATH; _z --add `pwd -P`"
 end
+
+set fish_color_autosuggestion 555
+set fish_color_command 00ff00
+set fish_color_comment 4e4e4e
+set fish_color_cwd green
+set fish_color_cwd_root red
+set fish_color_end 00FF00
+set fish_color_error d75f5f
+set fish_color_escape cyan
+set fish_color_history_current cyan
+set fish_color_host normal
+set fish_color_match cyan
+set fish_color_normal normal
+set fish_color_operator cyan
+set fish_color_param 00afff
+set fish_color_quote 725000
+set fish_color_redirection normal
+set fish_color_search_match \x2d\x2dbackground\x3dpurple
+set fish_color_selection \x2d\x2dbackground\x3dpurple
+set fish_color_status red
+set fish_color_user green
+set fish_color_valid_path \x2d\x2dunderline
+set fish_key_bindings fish_default_key_bindings
+set fish_pager_color_completion normal
+set fish_pager_color_description 555\x1eyellow
+set fish_pager_color_prefix cyan
+set fish_pager_color_progress cyan
